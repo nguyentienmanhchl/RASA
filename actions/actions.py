effeec41e11dda73a01106eb7a8f5f65cf88b31a
nguyentienmanhchl <63583534+nguyentienmanhchl@users.noreply.github.com>
2022-06-26 20:42:12 +0700

    26/6

--- actions/actions.py
+++ actions/actions.py
@@ -33,45 +33,45 @@ import random
 import requests
 
 
-DATABASE = ["bún đậu mắm tôm",
-            "bún đậu nước mắm",
-            "bún cá",
-            "bún hải sản",
-            "cơm văn phòng",
-            "cơm sườn",
-            "xôi",
-            "bún ốc",
-            "mì vằn thắn",
-            "hủ tiếu",
-            "bún chả",
-            "bún ngan",
-            "ngan xào tỏi",
-            "bún bò huế",
-            "mì tôm hải sản",
-            "bánh mì trứng xúc xích rắc thêm ít ngải cứu",
-            "bánh mì trứng",
-            "bánh mì xúc xích",
-            "bánh mì pate"]
+# DATABASE = ["bún đậu mắm tôm",
+#             "bún đậu nước mắm",
+#             "bún cá",
+#             "bún hải sản",
+#             "cơm văn phòng",
+#             "cơm sườn",
+#             "xôi",
+#             "bún ốc",
+#             "mì vằn thắn",
+#             "hủ tiếu",
+#             "bún chả",
+#             "bún ngan",
+#             "ngan xào tỏi",
+#             "bún bò huế",
+#             "mì tôm hải sản",
+#             "bánh mì trứng xúc xích rắc thêm ít ngải cứu",
+#             "bánh mì trứng",
+#             "bánh mì xúc xích",
+#             "bánh mì pate"]
 
 
-class ActionRecommend(Action):
+# class ActionRecommend(Action):
 
-    def name(self) -> Text:
-        return "action_recommend"
+#     def name(self) -> Text:
+#         return "action_recommend"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        food = []
-        for i in range(2):
-            food_number = random.randrange(len(DATABASE))
-            food.append(DATABASE[food_number])
+#         food = []
+#         for i in range(2):
+#             food_number = random.randrange(len(DATABASE))
+#             food.append(DATABASE[food_number])
 
-        dispatcher.utter_message(
-            text="Em nghĩ hôm nay anh chị có thể thử món '{}' hoặc bên cạnh đó cũng có thể là món '{}' ạ".format(food[0], food[1]))
+#         dispatcher.utter_message(
+#             text="Em nghĩ hôm nay anh chị có thể thử món '{}' hoặc bên cạnh đó cũng có thể là món '{}' ạ".format(food[0], food[1]))
 
-        return []
+#         return []
 
 # class ActionAskPerson(Action):
 
