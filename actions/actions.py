bb6f85b24d2da23dba3a2aebebff994da0d89c58
nguyentienmanhchl <63583534+nguyentienmanhchl@users.noreply.github.com>
2022-06-27 08:55:23 +0700

    27/6

--- actions/actions.py
+++ actions/actions.py
@@ -139,18 +139,21 @@ class ActionOneCondition(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        input_data1 = next(tracker.get_latest_entity_values("X"), None)
-        input_data2 = next(tracker.get_latest_entity_values("predicate"), None)
-        input_data3 = next(tracker.get_latest_entity_values("object"), None)
+        try:
+            input_data1 = next(tracker.get_latest_entity_values("X"), None)
+            input_data2 = next(tracker.get_latest_entity_values("predicate"), None)
+            input_data3 = next(tracker.get_latest_entity_values("object"), None)
        
 
-        input_data = input_data1 + "~"+ input_data2 +"~"+input_data3
-        r = requests.get("http://localhost:8080/one_condition",data=input_data.encode('utf-8'))
+            input_data = input_data1 + "~"+ input_data2 +"~"+input_data3
+            r = requests.get("http://localhost:8080/one_condition",data=input_data.encode('utf-8'))
        
-        print(input_data)
-        dispatcher.utter_message(
-            text="{}".format(r.content.decode()))
-
+            print(input_data)
+            dispatcher.utter_message(
+                text="{}".format(r.content.decode()))
+        except:
+            dispatcher.utter_message(
+                text = "Tôi không biết.")
         return []    
 class ActionTwoConditions(Action):
     def name(self) -> Text:
@@ -159,21 +162,24 @@ class ActionTwoConditions(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        input_data1 = next(tracker.get_latest_entity_values("X"), None)
-        iterator1 = tracker.get_latest_entity_values("predicate")
-        iterator2 = tracker.get_latest_entity_values("object")
-        input_data2 = next(iterator1,None)
-        input_data3 = next(iterator2,None)
-        input_data4 = next(iterator1,None)
-        input_data5 = next(iterator2,None)
-       
+        try:
+            input_data1 = next(tracker.get_latest_entity_values("X"), None)
+            iterator1 = tracker.get_latest_entity_values("predicate")
+            iterator2 = tracker.get_latest_entity_values("object")
+            input_data2 = next(iterator1,None)
+            input_data3 = next(iterator2,None)
+            input_data4 = next(iterator1,None)
+            input_data5 = next(iterator2,None)       
 
-        input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4+"~"+input_data5
-        r = requests.get("http://localhost:8080/two_conditions",data=input_data.encode('utf-8'))
+            input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4+"~"+input_data5
+            r = requests.get("http://localhost:8080/two_conditions",data=input_data.encode('utf-8'))
        
-        print(input_data)
-        dispatcher.utter_message(
-            text="{}".format(r.content.decode()))
+            print(input_data)
+            dispatcher.utter_message(
+                text="{}".format(r.content.decode()))
+        except:
+            dispatcher.utter_message(
+                text = "Tôi không biết.")
 
         return []           
 
@@ -185,16 +191,16 @@ class ActionAskOneProperty(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        input_data1 = next(tracker.get_latest_entity_values("object"), None)
-        input_data2 = next(tracker.get_latest_entity_values("predicate"),None)
-
+        try:
+            input_data1 = next(tracker.get_latest_entity_values("object"), None)
+            input_data2 = next(tracker.get_latest_entity_values("predicate"),None)
+            input_data = input_data1 + "~"+ input_data2 
+            r = requests.get("http://localhost:8080/ask_one_property",data=input_data.encode('utf-8'))
        
-
-        input_data = input_data1 + "~"+ input_data2 
-        r = requests.get("http://localhost:8080/ask_one_property",data=input_data.encode('utf-8'))
-       
-        print(input_data)
-        dispatcher.utter_message(
-            text="{}".format(r.content.decode()))
-
+            print(input_data)
+            dispatcher.utter_message(
+                text="{}".format(r.content.decode()))
+        except:
+            dispatcher.utter_message(
+                text = "Tôi không biết.")
         return []          
