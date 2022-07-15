8e0569fb7a9c35bca11b9c2956906dafa6fa41ff
nguyentienmanhchl <63583534+nguyentienmanhchl@users.noreply.github.com>
2022-07-15 20:32:24 +0700

    15/7

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,10 @@
 from typing import Any, Iterator, Text, Dict, List
+from dateutil import parser
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 import requests
+from rasa_sdk.events import UserUtteranceReverted
 
     
 class ActionOneCondition(Action):
@@ -16,12 +18,13 @@ class ActionOneCondition(Action):
             input_data1 = next(tracker.get_latest_entity_values("X"), ".")
             input_data2 = next(tracker.get_latest_entity_values("predicate"), ".")
             input_data3 = next(tracker.get_latest_entity_values("object"), None)
-       
-
-            input_data = input_data1 + "~"+ input_data2 +"~"+input_data3
+            print(input_data3)
+            input_data4 = next(tracker.get_latest_entity_values("time"),".")
+            # if input_data4 != ".":
+            #     print(parser.parse(input_data4).month)
+            input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4
             r = requests.get("http://localhost:8080/one_condition",data=input_data.encode('utf-8'))
        
-            print(input_data)
             dispatcher.utter_message(
                 text="{}".format(r.content.decode()))
         except:
@@ -43,11 +46,10 @@ class ActionTwoConditions(Action):
             input_data3 = next(iterator2,None)
             input_data4 = next(iterator1,".")
             input_data5 = next(iterator2,None)       
-
+            print(input_data3)
             input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4+"~"+input_data5
             r = requests.get("http://localhost:8080/two_conditions",data=input_data.encode('utf-8'))
        
-            print(input_data)
             dispatcher.utter_message(
                 text="{}".format(r.content.decode()))
         except:
@@ -67,10 +69,12 @@ class ActionAskOneProperty(Action):
         try:
             input_data1 = next(tracker.get_latest_entity_values("object"), None)
             input_data2 = next(tracker.get_latest_entity_values("predicate"),".")
+            print(input_data1)
+
             input_data = input_data1 + "~"+ input_data2 
+
             r = requests.get("http://localhost:8080/ask_one_property",data=input_data.encode('utf-8'))
        
-            print(input_data)
             dispatcher.utter_message(
                 text="{}".format(r.content.decode()))
         except:
@@ -78,7 +82,7 @@ class ActionAskOneProperty(Action):
                 text = "Tôi không biết.")
         return []          
 
-class ActionAskOneProperty(Action):
+class ActionAskAll(Action):
     def name(self) -> Text:
         return  "action_all"
 
@@ -99,7 +103,7 @@ class ActionAskOneProperty(Action):
 
 
 
-class ActionAskOneProperty(Action):
+class ActionAskDenChua(Action):
     def name(self) -> Text:
         return  "action_ask_den_chua"
 
@@ -118,7 +122,7 @@ class ActionAskOneProperty(Action):
                 text = "Tôi không biết.")
         return []   
 
-class ActionAskOneProperty(Action):
+class ActionAskAboutArea(Action):
     def name(self) -> Text:
         return  "action_ask_about_area"
 
@@ -137,7 +141,7 @@ class ActionAskOneProperty(Action):
                 text = "Tôi không biết.")
         return [] 
 
-class ActionAskOneProperty(Action):
+class ActionAskArea(Action):
     def name(self) -> Text:
         return  "action_ask_area"
 
@@ -156,7 +160,7 @@ class ActionAskOneProperty(Action):
                 text = "Tôi không biết.")
         return []   
 
-class ActionAskOneProperty(Action):
+class ActionAskRelate(Action):
     def name(self) -> Text:
         return  "action_ask_relate"
 
@@ -174,3 +178,20 @@ class ActionAskOneProperty(Action):
             dispatcher.utter_message(
                 text = "Tôi không biết.")
         return []                   
+
+# class ActionFallback(Action):
+#     def name(self) -> Text:
+#         return  "utter_please_rephrase"
+
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+#         input_data = next(tracker.get_latest_entity_values("object"), None)           
+#         print(input_data)
+           
+        
+#         dispatcher.utter_message(
+#             text = "Xin lỗi mình không hiểu câu hỏi, bạn diễn đạt lại được không?")
+#         return [UserUtteranceReverted()]    
