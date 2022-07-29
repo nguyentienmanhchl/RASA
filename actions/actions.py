04a6f37ad8dbd93e548c298b06770cf8072a45b1
nguyentienmanhchl <63583534+nguyentienmanhchl@users.noreply.github.com>
2022-07-29 21:41:31 +0700

    29/7

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,10 @@
+from operator import ne
 from typing import Any, Iterator, Text, Dict, List
 from dateutil import parser
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from regex import I
 import requests
 from rasa_sdk.events import UserUtteranceReverted
 
@@ -17,12 +19,13 @@ class ActionOneCondition(Action):
         try:
             input_data1 = next(tracker.get_latest_entity_values("class"), ".")
             input_data2 = next(tracker.get_latest_entity_values("predicate"), ".")
-            input_data3 = next(tracker.get_latest_entity_values("object"), None)
-            print(input_data3)
+            input_data3 = next(tracker.get_latest_entity_values("object"), "x")
+            #print(input_data3)
             input_data4 = next(tracker.get_latest_entity_values("time"),".")
             # if input_data4 != ".":
             #     print(parser.parse(input_data4).month)
             input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4
+            print(input_data)
             r = requests.get("http://localhost:8080/one_condition",data=input_data.encode('utf-8'))
        
             dispatcher.utter_message(
@@ -43,11 +46,12 @@ class ActionTwoConditions(Action):
             iterator1 = tracker.get_latest_entity_values("predicate")
             iterator2 = tracker.get_latest_entity_values("object")
             input_data2 = next(iterator1,".")
-            input_data3 = next(iterator2,None)
+            input_data3 = next(iterator2,"x")
             input_data4 = next(iterator1,".")
-            input_data5 = next(iterator2,None)       
-            print(input_data3)
+            input_data5 = next(iterator2,"x")       
+            #print(input_data3)
             input_data = input_data1 + "~"+ input_data2 +"~"+input_data3+"~"+input_data4+"~"+input_data5
+            print(input_data)
             r = requests.get("http://localhost:8080/two_conditions",data=input_data.encode('utf-8'))
        
             dispatcher.utter_message(
@@ -67,12 +71,16 @@ class ActionAskOneProperty(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data1 = next(tracker.get_latest_entity_values("object"), None)
+            iterator = tracker.get_latest_entity_values("object")
+            input_data1 = next(iterator,"x")
             input_data2 = next(tracker.get_latest_entity_values("predicate"),".")
-            print(input_data1)
-
-            input_data = input_data1 + "~"+ input_data2 
-
+            #print(input_data1)
+            if input_data1 != "x":
+                input_data3 = next(iterator,"x")
+            else:
+                input_data3 = "x"
+            input_data = input_data1 + "~"+ input_data2 +"~" +input_data3 
+            print(input_data)   
             r = requests.get("http://localhost:8080/ask_one_property",data=input_data.encode('utf-8'))
        
             dispatcher.utter_message(
@@ -91,7 +99,13 @@ class ActionAskAll(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data = next(tracker.get_latest_entity_values("object"), None)  
+            iterator = tracker.get_latest_entity_values("object")
+            input_data1 = next(iterator, "x")  
+            if input_data1 != "x":
+                input_data2 = next(iterator,"x")
+            else:
+                input_data2 = "x"
+            input_data = input_data1+"~"+input_data2
             print(input_data)         
             r = requests.get("http://localhost:8080/all",data=input_data.encode('utf-8'))       
             dispatcher.utter_message(
@@ -112,7 +126,7 @@ class ActionAskDenChua(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data = next(tracker.get_latest_entity_values("object"), None) 
+            input_data = next(tracker.get_latest_entity_values("object"),"x") 
             print(input_data)          
             r = requests.get("http://localhost:8080/ask_den_chua",data=input_data.encode('utf-8'))       
             dispatcher.utter_message(
@@ -131,7 +145,14 @@ class ActionAskAboutArea(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data = next(tracker.get_latest_entity_values("object"), None)   
+            iterator = tracker.get_latest_entity_values("object")
+            input_data1 = next(iterator, "x")  
+            if input_data1 != "x":
+                input_data2 = next(iterator,"x")
+            else:
+                input_data2 = "x"
+            input_data3 = next(tracker.get_latest_entity_values("predicate"),".")
+            input_data = input_data1+"~"+input_data2+"~"+input_data3 
             print(input_data)        
             r = requests.get("http://localhost:8080/ask_about_area",data=input_data.encode('utf-8'))       
             dispatcher.utter_message(
@@ -150,7 +171,7 @@ class ActionAskArea(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data = next(tracker.get_latest_entity_values("object"), None)
+            input_data = next(tracker.get_latest_entity_values("object"), "x")
             print(input_data)          
             r = requests.get("http://localhost:8080/ask_area",data=input_data.encode('utf-8'))       
             dispatcher.utter_message(
@@ -169,7 +190,9 @@ class ActionAskRelate(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         try:
-            input_data = next(tracker.get_latest_entity_values("object"), None)           
+            input_data1 = next(tracker.get_latest_entity_values("object"), "x")      
+            input_data2 = next(tracker.get_latest_entity_values("class"),".")  
+            input_data = input_data1+"~"+input_data2   
             print(input_data)
             r = requests.get("http://localhost:8080/ask_relate",data=input_data.encode('utf-8'))       
             dispatcher.utter_message(
